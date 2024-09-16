ed46fcd48f7c3b7949a8b14ef62d21cf5b063423
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-09-16 15:17:55 +0530

    java fundas

---
+++ src/com/deloitte/demo/ObjectLifeCycle.java
@@ -0,0 +1,43 @@
+package com.deloitte.demo;
+
+public class ObjectLifeCycle {
+
+	public static void main(String[] args) {
+
+		// creation - new
+		Person person = new Person("Sonu");
+
+		// usage
+		person.introduce();
+
+		// nullify 
+		person = null;
+
+	}
+
+}
+
+class Person {
+
+	String name;
+
+	public Person() {
+
+	}
+
+	public Person(String name) {
+		this.name = name;
+
+	}
+
+	void introduce() {
+		System.out.println("Hi! I'm " + name + ".");
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		System.out.println("object is being deleted.");
+
+	}
+
+}
