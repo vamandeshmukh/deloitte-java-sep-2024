50b390f7bc626eb850f744a517f396a1f2b37b2d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-09-16 14:38:57 +0530

    java fundas

--- src/com/deloitte/demo/StringDemo.java
+++ src/com/deloitte/demo/StringDemo.java
@@ -12,24 +12,32 @@ public class StringDemo {
 //		System.out.println(str);		
 //		System.out.println(System.identityHashCode(str));
 //		
-//		StringBuilder sb = new StringBuilder("abc");
-//		System.out.println(sb);
-//		System.out.println(System.identityHashCode(sb));
-//		
-//		sb.append("def");
-//		System.out.println(sb);
-//		System.out.println(System.identityHashCode(sb));
+		StringBuilder sb = new StringBuilder("abc");
+		System.out.println(sb);
+		System.out.println(System.identityHashCode(sb));
 		
-		String str = "Abc";
-		
-		String str2 = "ABC";
-		
-		System.out.println(str.equals(str2));
-		System.out.println(str.equalsIgnoreCase(str2));
-		
-		
+//		sb.append("def"); // abcdef   
+//		sb.replace(0, 3, "def"); // def 
+		sb.setLength(0);
+		sb.append("def");
+//		sb.
+		System.out.println(sb);
+		System.out.println(System.identityHashCode(sb));
 
+//		String str = "Abc";
+//		String str2 = "ABC";
+//
+//		System.out.println(str.equals(str2));
+//		System.out.println(str.equalsIgnoreCase(str2));
+//
+//		System.out.println(str.charAt(3));
 
 	}
 
 }
+
+
+
+
+
+
