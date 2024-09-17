8199d1c83293db642f6f91e30516f5557be3597e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-09-17 11:47:56 +0530

    java fundas 2

--- src/com/deloitte/demo/day2/ThisSuperDemo.java
+++ src/com/deloitte/demo/day2/ThisSuperDemo.java
@@ -1,11 +1,20 @@
 package com.deloitte.demo.day2;
 
+import java.util.Objects;
+
 public class ThisSuperDemo {
 
 	public static void main(String[] args) {
 
 		Employee emp = new Employee(101, "Sonu", 90.5);
 		System.out.println(emp.toString());
+
+		Employee emp2 = new Employee(101, "Sonu", 90.5);
+		System.out.println(emp2);
+
+		System.out.println(emp.hashCode());
+		System.out.println(emp2.hashCode());
+		System.out.println(emp.equals(emp2));
 	}
 }
 
@@ -15,7 +24,7 @@ class Person {
 
 	public Person() {
 	}
-	
+
 	public Person(String name) {
 		this.name = name;
 	}
@@ -32,6 +41,23 @@ class Employee extends Person {
 		super.name = name;
 		this.id = id;
 		this.salary = salary;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, salary);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return id == other.id && Double.doubleToLongBits(salary) == Double.doubleToLongBits(other.salary);
 	}
 
 	@Override
