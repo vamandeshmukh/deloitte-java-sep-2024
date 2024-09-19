7ea6be0c282d8c21d3b1f32b596bf90a1ead5a13
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-09-19 10:02:33 +0530

    jdbc

--- src/com/deloitte/demo/day4/jdbc/JdbcDemo.java
+++ src/com/deloitte/demo/day4/jdbc/JdbcDemo.java
@@ -7,6 +7,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JdbcDemo {
+	
+	// display id, name, salary of all the emps 
+	// handle exceptions 
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
